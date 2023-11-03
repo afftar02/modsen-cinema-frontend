1eb28eb1023fd09a45d3647dd1b9af21a9cda823
Steckij Nikita <stets119@gmail.com>
2023-11-03 11:57:07 +0300

    Footer unit test done

---
+++ src/components/Footer/Footer.test.tsx
@@ -0,0 +1,38 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import Footer from './index';
+import { THEMES } from 'constants/Themes';
+import { ThemeProvider } from 'styled-components';
+
+jest.mock('react-i18next', () => ({
+  useTranslation: () => ({
+    t: (key: string) => {
+      switch (key) {
+        case 'subscribe_text':
+          return 'Subscribe';
+        case 'subscribe_description_text':
+          return 'Join our newsletter to stay up to date on features and releases';
+        default:
+          break;
+      }
+    },
+  }),
+}));
+
+describe('Footer', () => {
+  it('Correct footer render', async () => {
+    render(
+      <ThemeProvider theme={THEMES.Dark}>
+        <Footer />
+      </ThemeProvider>
+    );
+
+    expect(screen.getByText('Subscribe')).toBeInTheDocument();
+    expect(
+      screen.getByText(
+        'Join our newsletter to stay up to date on features and releases'
+      )
+    ).toBeInTheDocument();
+    expect(screen.getByText('First column')).toBeInTheDocument();
+  });
+});
