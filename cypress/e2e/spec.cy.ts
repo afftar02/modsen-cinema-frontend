24a486827b31caeaedf31c00f114b6ad03a3bb9b
Steckij Nikita <stets119@gmail.com>
2023-11-10 20:15:45 +0300

    Home page e2e tests done

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,13 +1,31 @@
-describe('My First Test', () => {
-  it('Gets, types and asserts', () => {
+describe('Home Page', () => {
+  it('Buttons and links', () => {
     cy.visit('/');
 
-    cy.contains('type').click();
+    cy.contains('Home').should('have.css', 'color', 'rgb(217, 134, 57)');
+    cy.contains('Bookings').should('have.css', 'color', 'rgb(255, 255, 255)');
 
-    cy.url().should('include', '/commands/actions');
+    cy.contains('Bookings').click();
+    cy.url().should('include', '/signin');
+    cy.get('[data-cy="close"]').click();
 
-    cy.get('.action-email').type('fake@email.com');
+    cy.contains('Sign up').click();
+    cy.url().should('include', '/signup');
+    cy.get('[data-cy="close"]').click();
 
-    cy.get('.action-email').should('have.value', 'fake@email.com');
+    cy.contains('Sign in').click();
+    cy.url().should('include', '/signin');
+    cy.get('[data-cy="close"]').click();
+
+    cy.get('[data-cy="settings"]').click();
+    cy.contains('Choose settings:');
+    cy.get('[data-cy="close"]').click();
+
+    cy.get('[data-cy="play-preview"]').click();
+    cy.get('video').should('exist');
+    cy.get('[data-cy="close"]').click();
   });
 });
+// cy.get('.action-email').type('fake@email.com');
+//
+// cy.get('.action-email').should('have.value', 'fake@email.com');
