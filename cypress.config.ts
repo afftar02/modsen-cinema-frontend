c6b7951d60534970c2045b9a1d6f497f430773c0
Steckij Nikita <stets119@gmail.com>
2023-11-10 20:35:55 +0300

    Settings e2e tests done

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
-    // baseUrl: 'https://cinema-app-sigma.vercel.app',
     baseUrl: 'http://localhost:3000',
   },
   viewportWidth: 1920,
--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -26,6 +26,42 @@ describe('Home Page', () => {
     cy.get('[data-cy="close"]').click();
   });
 });
+
+describe('Settings', () => {
+  it('Language changing', () => {
+    cy.visit('/');
+
+    cy.get('[data-cy="settings"]').click();
+
+    cy.contains('Russian').click();
+    cy.contains('Apply').click();
+
+    cy.contains('Главная');
+    cy.contains('Билеты');
+    cy.contains('Регистрация');
+    cy.contains('Вход');
+  });
+  it('Theme changing', () => {
+    cy.visit('/');
+
+    cy.get('[data-cy="settings"]').click();
+
+    cy.contains('Light').click();
+    cy.contains('Apply').click();
+
+    cy.get('[data-cy="theme-wrapper"]').should(
+      'have.css',
+      'background-color',
+      'rgb(255, 255, 255)'
+    );
+  });
+});
+
+// describe('Auth forms', () => {
+//   it('', () => {
+//     cy.visit('/signin');
+//   });
+// });
 // cy.get('.action-email').type('fake@email.com');
 //
 // cy.get('.action-email').should('have.value', 'fake@email.com');
