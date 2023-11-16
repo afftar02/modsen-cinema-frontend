f916af7b0fc2c948db588576b8e13a1b89dcdeb2
Steckij Nikita <stets119@gmail.com>
2023-11-16 09:26:32 +0300

    Gender input replaced by select

--- src/components/EditProfileModal/index.tsx
+++ src/components/EditProfileModal/index.tsx
@@ -6,14 +6,15 @@ import ErrorFallback from 'components/ErrorFallback';
 import ModalPortal from 'components/ModalPortal';
 import { motion } from 'framer-motion';
 import CloseIcon from 'components/CloseIcon';
-import { ChangeEvent, useCallback, useState } from 'react';
+import { ChangeEvent, useCallback, useRef, useState } from 'react';
 import { uploadAvatar } from 'services/avatarService';
 import { AuthContextType, useAuth } from 'auth/Auth';
 import { updateUser } from 'services/userService';
 import { UserType } from 'types/User';
 import { useTranslation } from 'react-i18next';
 import { Button, FileInput, Input, PasswordInput } from 'modsen-library';
-import OutsideHandler from 'components/OutsideHandler';
+import { useClickOutside } from 'hooks/useClickOutside';
+import GenderSelect from 'components/GenderSelect';
 
 type EditProfileFormProps = {
   onClose: () => void;
@@ -78,8 +79,11 @@ function EditProfileModal({ onClose }: EditProfileFormProps) {
   const { t } = useTranslation();
   const theme = useTheme();
   const [avatar, setAvatar] = useState<File>();
+  const modalRef = useRef(null);
 
   const { loadUser } = useAuth() as AuthContextType;
+
+  useClickOutside(modalRef, onClose);
 
   const formik = useFormik({
     initialValues: {
@@ -136,78 +140,70 @@ function EditProfileModal({ onClose }: EditProfileFormProps) {
 
   return (
     <ModalPortal isFixed>
-      <OutsideHandler onClick={onClose}>
-        <Modal
-          initial={{ scale: 0.5, opacity: 0 }}
-          animate={{ scale: 1, opacity: 1 }}
-          exit={{ scale: 0.5, opacity: 0 }}
-          transition={{
-            duration: 0.3,
-          }}
-        >
-          <CloseIcon onClick={onClose} />
-          <ErrorBoundary fallback={<ErrorFallback />}>
-            <TextBlock>
-              <span>{t('edit_profile_title')}</span>
-            </TextBlock>
-            <StyledForm onSubmit={formik.handleSubmit}>
-              <FileInput
-                value={avatar?.name}
-                onChange={handleFileUpload}
-                uploadText={t('upload_avatar_text')}
-                uploadedText={t('uploaded_file_text')}
-                textColor={theme.color}
-                borderColor={theme.color}
-                uploadedButtonBgColor={theme.buttonBgColor}
-                uploadedButtonColor={theme.buttonColor}
-              />
-              <Input
-                placeholder={formik.errors.name ?? t('edit_name_placeholder')}
-                onChange={formik.handleChange}
-                value={formik.values.name}
-                onClick={() => formik.setFieldError('name', undefined)}
-                name={'name'}
-                isError={!!formik.errors.name}
-                color={theme.color}
-              />
-              <Input
-                placeholder={
-                  formik.errors.surname ?? t('edit_surname_placeholder')
-                }
-                onChange={formik.handleChange}
-                value={formik.values.surname}
-                onClick={() => formik.setFieldError('surname', undefined)}
-                name={'surname'}
-                isError={!!formik.errors.surname}
-                color={theme.color}
-              />
-              <Input
-                placeholder={
-                  formik.errors.gender ?? t('edit_gender_placeholder')
-                }
-                onChange={formik.handleChange}
-                value={formik.values.gender}
-                onClick={() => formik.setFieldError('gender', undefined)}
-                name={'gender'}
-                isError={!!formik.errors.gender}
-                color={theme.color}
-              />
-              <PasswordInput
-                withIcon={false}
-                placeholder={
-                  formik.errors.password ?? t('edit_password_placeholder')
-                }
-                onChange={formik.handleChange}
-                value={formik.values.password}
-                onClick={() => formik.setFieldError('password', undefined)}
-                isError={!!formik.errors.password}
-                color={theme.color}
-              />
-              <SubmitButton type={'submit'}>{t('save_text')}</SubmitButton>
-            </StyledForm>
-          </ErrorBoundary>
-        </Modal>
-      </OutsideHandler>
+      <Modal
+        ref={modalRef}
+        initial={{ scale: 0.5, opacity: 0 }}
+        animate={{ scale: 1, opacity: 1 }}
+        exit={{ scale: 0.5, opacity: 0 }}
+        transition={{
+          duration: 0.3,
+        }}
+      >
+        <CloseIcon onClick={onClose} />
+        <ErrorBoundary fallback={<ErrorFallback />}>
+          <TextBlock>
+            <span>{t('edit_profile_title')}</span>
+          </TextBlock>
+          <StyledForm onSubmit={formik.handleSubmit}>
+            <FileInput
+              value={avatar?.name}
+              onChange={handleFileUpload}
+              uploadText={t('upload_avatar_text')}
+              uploadedText={t('uploaded_file_text')}
+              textColor={theme.color}
+              borderColor={theme.color}
+              uploadedButtonBgColor={theme.buttonBgColor}
+              uploadedButtonColor={theme.buttonColor}
+            />
+            <Input
+              placeholder={formik.errors.name ?? t('edit_name_placeholder')}
+              onChange={formik.handleChange}
+              value={formik.values.name}
+              onClick={() => formik.setFieldError('name', undefined)}
+              name={'name'}
+              isError={!!formik.errors.name}
+              color={theme.color}
+            />
+            <Input
+              placeholder={
+                formik.errors.surname ?? t('edit_surname_placeholder')
+              }
+              onChange={formik.handleChange}
+              value={formik.values.surname}
+              onClick={() => formik.setFieldError('surname', undefined)}
+              name={'surname'}
+              isError={!!formik.errors.surname}
+              color={theme.color}
+            />
+            <GenderSelect
+              onChange={formik.handleChange}
+              value={formik.values.gender}
+            />
+            <PasswordInput
+              withIcon={false}
+              placeholder={
+                formik.errors.password ?? t('edit_password_placeholder')
+              }
+              onChange={formik.handleChange}
+              value={formik.values.password}
+              onClick={() => formik.setFieldError('password', undefined)}
+              isError={!!formik.errors.password}
+              color={theme.color}
+            />
+            <SubmitButton type={'submit'}>{t('save_text')}</SubmitButton>
+          </StyledForm>
+        </ErrorBoundary>
+      </Modal>
     </ModalPortal>
   );
 }
---
+++ src/components/GenderSelect/index.tsx
@@ -0,0 +1,80 @@
+import { styled, useTheme } from 'styled-components';
+import { Icon } from 'modsen-library';
+import { useTranslation } from 'react-i18next';
+import { ChangeEventHandler } from 'react';
+
+type GenderSelectProps = {
+  onChange: ChangeEventHandler<HTMLSelectElement>;
+  value: string;
+};
+
+const StyledSelect = styled.select<{
+  $color: string;
+  $optionBgColor: string;
+  $selectHoverColor: string;
+}>`
+  appearance: none;
+  /* safari */
+  -webkit-appearance: none;
+  background-color: transparent;
+  font-family: 'Poppins', sans-serif;
+  font-size: 20px;
+  font-weight: 300;
+  color: ${(props) => props.$color};
+  outline: none;
+  padding: 5px 0 5px 15px;
+  cursor: pointer;
+  transition: all 0.1s ease-in-out;
+  width: 100%;
+
+  &:hover {
+    background-color: ${(props) => props.$selectHoverColor};
+  }
+
+  & option {
+    background-color: ${(props) => props.$optionBgColor};
+  }
+`;
+
+const SelectContainer = styled.div`
+  position: relative;
+`;
+
+const SelectArrow = styled(Icon)`
+  transform: rotate(90deg);
+  position: absolute;
+  right: 15px;
+  top: 10px;
+`;
+
+function GenderSelect({ onChange, value }: GenderSelectProps) {
+  const { t } = useTranslation();
+  const theme = useTheme();
+
+  return (
+    <SelectContainer>
+      <SelectArrow
+        id={'right-arrow-nested'}
+        height={20}
+        width={20}
+        viewBox="0 0 24 24"
+      />
+      <StyledSelect
+        onChange={onChange}
+        value={value}
+        name={'gender'}
+        $color={theme.color}
+        $optionBgColor={theme.bgColor}
+        $selectHoverColor={theme.selectHoverColor}
+      >
+        <option defaultChecked hidden>
+          {t('edit_gender_placeholder')}
+        </option>
+        <option value="MALE">{t('male')}</option>
+        <option value="FEMALE">{t('female')}</option>
+      </StyledSelect>
+    </SelectContainer>
+  );
+}
+
+export default GenderSelect;
